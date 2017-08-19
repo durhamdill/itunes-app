bea9d074667bd0ca3d65650028c14984c8e4c643
Katy Dillard <durhamdill@gmail.com>
2017-08-19 18:22:28 -0400

    fix audio play on click/css updates

--- main.js
+++ main.js
@@ -8,11 +8,40 @@ let url = "https://itunes.apple.com/search?term=";
 
 let submitbutton = document.getElementById("submitbutton");
 
-let searchSongs = "&entity=song&limit=10";
+let searchSongs = "&entity=song&limit=50";
 
 let searchText = document.getElementById("searchText");
 
-let songArray = [];
+let searchData = [];
+
+let startSearchUrl = "https://itunes.apple.com/search?term=love&entity=song&limit=50&sort=recent";
+
+  fetch(startSearchUrl)
+    .then(
+      function(response) {
+        // error checking
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' + response.status);
+          return
+        }
+
+        response.json().then(function(data) {
+          searchData = data.results;
+          results = document.querySelector(".results");
+          results.innerHTML= "";
+
+            for (i=0; i<searchData.length; i++){
+              results.innerHTML += `
+              <div class="test">
+                <img id=${i} class="image" src="${searchData[i].artworkUrl100}">
+                <h2 id=${i}>${searchData[i].trackName}</h2>
+                <p id=${i}>${searchData[i].artistName}</p>
+              </div>
+              `
+            };
+        })
+      }
+      )
 
 
 // 2. Create your `submit` event for getting the user's search term
@@ -37,54 +66,52 @@ function itunesSearch() {
         }
 
         response.json().then(function(data) {
-          console.log(data.results.length);
-          console.log(data.results);
+          // console.log(data.results.length);
+          // console.log(data.results);
+          searchData = data.results;
           results = document.querySelector(".results");
           results.innerHTML= "";
 
-          if (data.results.length===0){
+          if (searchData.length===0){
             searchText.innerHTML=`<p>Sorry, your search for "${input}" did not return any results. Try again!</p>`
           }
           else {
-            searchText.innerHTML=`Search results for "${input}":`;
+            // searchText.innerHTML=`Search results for "${input}":`;
             // data.results.map((song)=>
-            for (let i=0; i<data.results.length; i++){
-              songArray.push(data.results[i].previewUrl);
+            for (i=0; i<searchData.length; i++){
               results.innerHTML += `
-              <div class="test" id=${i} onclick=playSong()>
-                <img class="image" src="${data.results[i].artworkUrl100}" value="${data.results[i].previewUrl}">
-                <h2>${data.results[i].trackName}</h2>
-                <p>${data.results[i].artistName}</p>
+              <div class="test">
+                <img id=${i} class="image" src="${searchData[i].artworkUrl100}">
+                <h2 id=${i}>${searchData[i].trackName}</h2>
+                <p id=${i}>${searchData[i].artistName}</p>
               </div>
               `
-
             };
           }
+          // console.log(searchData);
           // let songBox=document.getElementsByClassName("test");
           // songBox.addEventListener("click", playSong, false);
-          console.log("array of songUrls: " + songArray);
         })
       }
       )
     }
 
+
 // 3. Create your `fetch` request that is called after a submission
 // 4. Create a way to append the fetch results to your page
 // 5. Create a way to listen for a click that will play the song in the audio play
 
-// let results = document.querySelector(".results");
+let results = document.querySelector(".results");
+results.addEventListener("click", playSong, false);
 let musicPlayer = document.querySelector(".music-player");
-// results.addEventListener("click", playSong, false);
+// let nowPlayText = document.querySelector("#nowPlayText");
+let nowPlayText = document.getElementById("nowPlayText");
 
-//   function playSong(e) {
-//     if (e.target !== e.currentTarget) {
-//         // var clickedItem = e.target.id;
-//         // alert("Hello " + clickedItem);
-//         console.log(target);
-//     }
-//     e.stopPropagation();
-// }
-
-function playSong() {
-  console.log(this.id);
+function playSong(e) {
+  // let test=document.getElementsByClassName(test);
+  // console.log(e.target.id);
+  // console.log(searchData);
+  let i = e.target.id;
+  nowPlayText.innerHTML=`Now playing "${searchData[i].trackName}" by ${searchData[i].artistName}:`;
+  musicPlayer.setAttribute('src', searchData[i].previewUrl);
 }
